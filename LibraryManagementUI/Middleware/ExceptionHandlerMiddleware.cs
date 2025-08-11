71c717bcd7aff38aacc53cae4e18a644c73b2fcb
Niranjan <iniranjankumarsingh.gmail.com>
2025-08-11 12:49:20 +0530

    Added Exception Middleware

---
+++ LibraryManagementUI/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,33 @@
+﻿
+namespace LibraryManagementUI.Middleware
+{
+    public class ExceptionHandlerMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+               await next(context);
+            }
+            catch(Exception ex)
+            {
+                // Set the response status code and content
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Response.ContentType = "application/json";
+                // Return a generic error message
+                await context.Response.WriteAsync(new
+                {
+                    StatusCode = context.Response.StatusCode,
+                    Message = "An unexpected error occurred. Please try again later."
+                }.ToString());
+            }
+        }
+    }
+    public static class ExceptionHandlerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
